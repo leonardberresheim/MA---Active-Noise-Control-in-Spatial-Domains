6f67ae9bdbc490f2ccc9b2c0fe64ec1bf0113a98
Leonard Emil Alexander Berresheim <berreshe@htw-berlin.de>
2021-11-03 08:15:05 +0100

    Reorganize matlab folder and implement 3d kwave

--- Matlab/ISM_ego/point_image_source.m
+++
@@ -1,4 +0,0 @@
-function point_image_source(p_source, p_wall, image_order, p_receiver, frequency, dimention, spacing)
-    p_source = image_source_order(p_source, p_wall, image_order);
-    point_source(p_source, p_receiver, frequency, dimention, spacing);
-end
